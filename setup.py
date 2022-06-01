fbfcbab3b2af20ac3ffc4725bc8edcd15d09cbb9
Hady Hammad <36478378+HadyKh@users.noreply.github.com>
2022-06-01 15:49:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='learning is very effective in detecting the DDoS',
-    author='Group project 8 (Hady Hammad, Lobna Elsaid, Abdallah Rashed, Ragab Godah)',
+    author='(Hady Hammad, Lobna Elsaid, Abdallah Rashed, Ragab Godah)',
     license='',
 )
