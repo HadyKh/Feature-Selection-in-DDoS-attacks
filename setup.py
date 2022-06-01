ad4f305bb0dad3a60c8b5822a571a38e20ebac23
Hady Hammad <36478378+HadyKh@users.noreply.github.com>
2022-06-01 15:50:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     version='0.1.0',
     description='learning is very effective in detecting the DDoS',
     author='(Hady Hammad, Lobna Elsaid, Abdallah Rashed, Ragab Godah)',
-    license='',
+    license='MIT License',
 )
